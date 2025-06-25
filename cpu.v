8454a630b25ea2749224deb26ea858d7ede73523
ZaidX88 <zaid0731alpha@gmail.com>
2025-06-25 09:20:16 +0530

    add 256x8-bit data memory module with read/write functionality

--- cpu.v
+++ cpu.v
@@ -1,6 +1,7 @@
 `include "alu.v"
 `include "regFile.v"
 `include "bit8Shifter.v"
+`include "memory.v"
 
 module regPC (PC, RESET, CLK, ADDRESS);  //Program Counter that incremented by 4
     
@@ -157,21 +158,25 @@ module cpu (PC, INSTRUCTION, CLK, RESET);
     input CLK, RESET;
 
     reg [7:0] OPCODE, RD, RT, RS;  //Instruction -> | OPCODE | RD | RT | RS/IMM |
-    wire WRITEENABLE, S_MUX1, S_MUX2, SHIFT_MUX, BRANCH, JUMP, ZERO, ANDOUT, OROUT, XOROUT;    // Signals for write enable, MUX1 select, MUX2 select
+    wire REG_WRITE_ENABLE, S_MUX1, S_MUX2, SHIFT_MUX, BRANCH, JUMP, ZERO, ANDOUT, OROUT, XOROUT,
+        MREAD_SIG, MWRITE_SIG, BUSYWAIT, MEM_ALU_MUX_SIG, MEM_WRITE_ENABLE, MEM_READ_ENABLE;    // Signals for write enable, MUX1 select, MUX2 select
     wire [2:0] ALUOP, SHIFT_OP;                   // ALU operation signal line
-    wire [7:0] OUT1, OUT2, COUT, MUX1_OUT, MUX2_OUT, SHIFT_MUX_OUT, RESULT, SHIFTED_VALUE; // register file out1,out2, complemented value, MUX1 output,MUX2 output, ALU result
+    wire [7:0] OUT1, OUT2, COUT, MUX1_OUT, MUX2_OUT, SHIFT_MUX_OUT, RESULT, SHIFTED_VALUE,
+        MDREAD, MEM_ALU_MUX; // register file out1,out2, complemented value, MUX1 output,MUX2 output, ALU result
     reg [2:0] INADDRESS, OUT1ADDRESS, OUT2ADDRESS; // Address lines for register file
     reg BRANCH_SIGN;
     
     wire [31:0] PCADDR, C_FOUR, PCADDROUT, TARGETADDROUT, EADDR;
 
     regPC myRegPC(PC, RESET, CLK, PCADDR);
-    controlUnit myControlUnit(WRITEENABLE, ALUOP, SHIFT_OP, S_MUX1, S_MUX2,SHIFT_MUX, BRANCH, JUMP, OPCODE);
-    reg_file myRegFile(OUT1, OUT2, RESULT, INADDRESS, OUT1ADDRESS, OUT2ADDRESS, WRITEENABLE, CLK, RESET);
+    controlUnit myControlUnit(REG_WRITE_ENABLE, ALUOP, SHIFT_OP, S_MUX1, S_MUX2,SHIFT_MUX, BRANCH, JUMP, OPCODE);
+    reg_file myRegFile(OUT1, OUT2, MEM_ALU_MUX, INADDRESS, OUT1ADDRESS, OUT2ADDRESS, WRITEENABLE, CLK, RESET);
     complement myComplement(COUT, OUT2);
     alu myAlu(ZERO, RESULT, OUT1, SHIFT_MUX_OUT, ALUOP);
 
     b8_shifter shifter(OUT1,RS[2:0], SHIFT_OP, SHIFTED_VALUE);
+
+    data_memory myDataMemory(CLK, RESET, MEM_READ_ENABLE, MEM_WRITE_ENABLE, RESULT, OUT1, MDREAD , BUSYWAIT); // Data memory module
 
     addr myPcAddr(PCADDROUT, PC, C_FOUR); 
     addr myTargetAddr(TARGETADDROUT, PCADDROUT, EADDR);
@@ -185,6 +190,7 @@ module cpu (PC, INSTRUCTION, CLK, RESET);
     assign SHIFT_MUX_OUT = SHIFT_MUX ? SHIFTED_VALUE : MUX2_OUT;
     assign C_FOUR = 4;
     assign PCADDR = OROUT ? TARGETADDROUT : PCADDROUT;
+    assign MEM_ALU_MUX = MEM_ALU_MUX_SIG ? MDREAD : RESULT;
 
     always @(*) begin
         OPCODE = INSTRUCTION[31:24]; //Opcode Extraction
